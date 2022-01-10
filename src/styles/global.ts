201f641b2edbaa359078e2d693a15a2aa5b1f245
Ramon Seugling <ramonseugling@gmail.com>
2022-01-10 07:52:32 -0300

    Dashboard and summary components added

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
     :root {
         --backgroud: #F0F2F5;
         --red: #E52E4D;
+        --green: #33cc95;
         --blue: #5429CC;
 
         --blue-light: #6933FF;
