d87aaa6e1888e1c2b146e4f3222f0e93a28c4dfe
Ramon Seugling <ramonseugling@gmail.com>
2022-01-12 15:26:09 -0300

    Miragejs and axios configured

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = "api"
+
+    this.get('/transactions', () => {
+      return {
+        transactions: [
+          { id: 1, name: "Inception", year: 2010 },
+          { id: 2, name: "Interstellar", year: 2014 },
+          { id: 3, name: "Dunkirk", year: 2017 },
+        ],
+      }
+    })
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
