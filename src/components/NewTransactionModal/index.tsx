b72c6a1be080c09094e51faefee3dfda0d6b5fe8
Ramon Seugling <ramonseugling@gmail.com>
2022-01-20 22:23:13 -0300

    Transaction added to transaction table by context

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -19,15 +19,21 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     const [amount, setAmount] = useState(0);
     const [type, setType] = useState('deposit');
 
-    function handleCreateNewTransaction(event: FormEvent) {
+    async function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        createTransaction({
+        await createTransaction({
             title, 
             amount, 
             category,
             type,
         })
+        
+        setType('');
+        setCategory('');
+        setAmount(0);
+        setType('deposit');
+        onRequestClose();
     }
 
     return (
